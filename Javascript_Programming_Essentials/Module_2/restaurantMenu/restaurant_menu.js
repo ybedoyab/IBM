4e3b79daab651063a178a5c7683df05896205405
Yulian Bedoya <ybedoyab@unal.edu.co>
2024-07-09 13:46:22 -0500

    estructura y restaurantMenu

---
+++ Javascript_Programming_Essentials/Module_2/restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $30', 'Pasta - $34', 'Burger - $12', 'Salmon - $65'];
+const dessertMenu = ['Cake - $90', 'Ice Cream - $23', 'Pudding - $12', 'Fruit Salad - $12'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
