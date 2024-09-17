da35984edff58b00a56aa8601318efb695d2318e
Yulian Bedoya <ybedoyab@unal.edu.co>
2024-09-16 19:47:20 -0500

    module 2

---
+++ Developing_Back-End_Apps_with_Node.js_and_Express/Module_2/practice2.js
@@ -0,0 +1,19 @@
+//Change the code to call the promises sequentially and see how the output changes.
+
+let myPromise1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Promise 1 resolved")
+    },6000)})
+
+let myPromise2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Promise 2 resolved")
+    },3000)})
+
+  myPromise1.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+  })
+
+  myPromise2.then((successMessage) => {
+  console.log("From Callback " + successMessage)
+})
